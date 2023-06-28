c1ae728346c0f9447aa5363b6b3b501b4dc003b1
Djekson <alekssyvorov29@gmail.com>
2023-06-28 20:57:47 +0300

    home work 4

--- main.py
+++ main.py
@@ -1,60 +1,36 @@
 def task_1():
-    num_1 = int(input('Number 1 '))
-    num_2 = int(input('Number 2 '))
-    num_3 = int(input('Number 3 '))
-    choice_user = input('Summa - 1/Multiplication - 2')
-    match choice_user:
-        case '1':
-            return num_1 + num_2 + num_3
-        case '2':
-            return num_1 * num_2 * num_3
+    num_1 = int(input('Number '))
+    print(num_1 // 10 % 10)
+    print(num_1 % 10)
 
 
 def task_2():
-    num_1 = int(input('Number 1 '))
-    num_2 = int(input('Number 2 '))
-    num_3 = int(input('Number 3 '))
-    choice_user = input('Max - 1\nMin - 2\nAverage - 3')
-    match choice_user:
-        case '1':
-            if num_1 > num_2 and num_1 > num_3:
-                return num_1
-            elif num_2 > num_1 and num_2 > num_3:
-                return num_2
-            elif num_3 > num_1 and num_3 > num_2:
-                return num_3
-            else:
-                return f'Number is equal'
-        case '2':
-            if num_1 < num_2 and num_1 < num_3:
-                return num_1
-            elif num_2 < num_1 and num_2 < num_3:
-                return num_2
-            elif num_3 < num_1 and num_3 < num_2:
-                return num_3
-            else:
-                return f'Number is equal'
-        case '3':
-            return (num_1 + num_2 + num_3) / 3
+    num_1 = int(input('Number '))
+    print(num_1 // 100)
+    print(num_1 // 10)
+    print(num_1 % 10)
+    print(num_1 // 100 + num_1 // 10 + num_1 % 10)
 
 
 def task_3():
-    meters = float(input('Input meters '))
-    choice_user = input('milli - 1\ninches - 2\nyards - 3')
-    if choice_user == '1':
-        return int(meters) * 0.000621371 + (meters - int(meters)) * 0.000621371
-    elif choice_user == '2':
-        return int(meters) * 25.4 + (meters - int(meters)) * 25.4
-    else:
-        return int(meters) * 1.09361 + (meters - int(meters)) * 1.09361
+    num_1 = input('Number 1')
+    num_2 = input('Number 2')
+    return int(num_1 + num_2)
+
+
+def task_4():
+    temperature_c = float(input('Input temperature '))
+    return 9 / 5 * temperature_c + 32
 
 
 if __name__ == "__main__":
-    choice = int(input('Make you choice 1/2/3 '))
+    choice = int(input('Make you choice 1/2/3/4 '))
     match choice:
         case 1:
-            print(task_1())
+            task_1()
         case 2:
             print(task_2())
         case 3:
             print(task_3())
+        case 4:
+            print(task_4())
