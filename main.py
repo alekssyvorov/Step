c7bf29990830aed6430cdec534ac1fb13b97b9f9
Djekson <alekssyvorov29@gmail.com>
2023-06-29 20:32:01 +0300

    home work 8

--- main.py
+++ main.py
@@ -1,61 +1,75 @@
-def task_1(number: int):
-    if number % 2 == 0:
-        print(number, 'Even number.')
-    else:
-        print(number, 'Odd number.')
+import math
 
 
-def task_2(number: int):
-    if number % 7 == 0:
-        print('Number is a multiple of 7.')
-    else:
-        print('Number is not a multiple of 7.')
+def task_1(time_second: int, choice: str) -> int:
+    match choice.lower():
+        case 'h':
+            return int(24 - time_second / 3600)
+        case 'm':
+            return int(1440 - time_second / 360)
+        case 's':
+            return 86400 - time_second
 
 
-def task_3(number1: float, number2: float):
-    if number1 > number2:
-        print(number1)
-    else:
-        print(number2)
+def task_2(diam: float, choice_user: str) -> float:
+    match choice_user.lower():
+        case 's':
+            return math.pi * math.sqrt(diam / 2)
+        case 'p':
+            return math.pi * diam
 
 
-def task_4(number1: float, number2: float):
-    if number1 < number2:
-        print(number1)
-    else:
-        print(number2)
+def task_3(price: float, count: int, discount: int, choice_user: str) -> float:
+    match choice_user.lower():
+        case '1':
+            return price - price * discount / 100
+        case 'all':
+            return count * (price - price * discount / 100)
 
 
-def task_5(number1: float, number2: float, sign: str):
-    if sign == '+':
-        print(number1 + number2)
-    elif sign == '-':
-        print(number1 - number2)
-    elif sign == 'average':
-        print(2 / (number1 + number2))
-    elif sign == '*':
-        print(number1 * number2)
+def task_4(size: float, speed: int, choice: str) -> int:
+    match choice.lower():
+        case 'h':  # 2  100 200/c
+            return int(size * 8e+9 / speed / 360)
+        case 'm':
+            return int(size * 8e+9 / speed / 60)
+        case 's':
+            return int(size * 8e+9 / speed)
+
+
+def task_5(t_d: int) -> str:
+    if 0 < t_d < 6:
+        return f'Good Night'
+    elif 6 <= t_d < 13:
+        return f'Good Morning'
+    elif 13 <= t_d < 17:
+        return f'Good Day'
+    elif 17 <= t_d <= 24 or t_d == 0:
+        return 'Good Evening'
 
 
 if __name__ == "__main__":
     choice = int(input('Make you choice 1/2/3/4/5 '))
     match choice:
         case 1:
-            number = int(input('Enter the number: '))
-            task_1(number)
+            time_second = int(input('Enter the second: '))
+            choice = input('Input h/m/s ')
+            print(task_1(time_second, choice))
         case 2:
-            number = int(input('Enter the number: '))
-            task_2(number)
+            d = float(input('Enter the diameter: '))
+            choice = input('Input s/p ')
+            print(task_2(d, choice))
         case 3:
-            number1 = float(input('Enter the first number: '))
-            number2 = float(input('Enter the second number: '))
-            task_3(number1, number2)
+            price = float(input('Enter the price : '))
+            count = int(input('Enter the quantity: '))
+            discount = int(input('Enter the discount: '))
+            choice = input('Make your choice 1/all')
+            print(task_3(price, count, discount, choice))
         case 4:
-            number1 = float(input('Enter the first number: '))
-            number2 = float(input('Enter the second number: '))
-            task_4(number1, number2)
+            size = float(input('Enter the size file: '))
+            speed = int(input('Enter the speed: '))
+            choice = input('Make your choice h/m/s: ')
+            print(task_4(size, speed, choice))
         case 5:
-            number1 = int(input('Enter the first number: '))
-            number2 = int(input('Enter the second number: '))
-            sign = input('Enter the sign: +-average')
-            task_5(number1, number2, sign)
+            time_day = int(input('Enter the time of day: '))
+            print(task_5(time_day))
